699d9853008c1836d58d7f7c79855c74449ee082
erwin-beckers <info@erwinbeckers.nl>
2014-05-10 15:47:08 +0200

    add default.aspx
    forgot to add the default.aspx

---
+++ iDeal.Example/Default.aspx
@@ -0,0 +1,43 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="iDeal.Example.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h1> Commands:</h1> 
+            <asp:Button runat="server" ID="btnDirReq" Text="DirectoryRequest" OnClick="btnDirReq_Click" /> <br />
+            <asp:Button runat="server" ID="btnTransaction1" Text="Transaction" OnClick="btnTransaction1_Click" /><br />
+            <asp:Button runat="server" ID="btnStatus1" Text="Status" OnClick="btnStatus1_Click" /><br />
+            Amount:<input runat="server" id="txtAmount" value="1" />euro<br />
+        </div>
+        <hr />
+        <h2>Results:</h2>
+
+         <div runat="server" id="logResult"></div>
+
+        <table runat="server" ID="tableError">
+            <tr>
+                <td>Error Code</td>
+                <td><asp:Label runat="server" ID="lblErrorCode"></asp:Label></td>
+            </tr>
+            <tr>
+                <td>Error Message</td>
+                <td><asp:Label runat="server" ID="lblErrorMessage"></asp:Label></td>
+            </tr>
+            <tr>
+                <td>Error Detail</td>
+                <td><asp:Label runat="server" ID="lblErrorDetail"></asp:Label></td>
+            </tr>
+            <tr>
+                <td>Consumer Message</td>
+                <td><asp:Label runat="server" ID="lblConsumerMessage"></asp:Label></td>
+            </tr>
+        </table>
+    </form>
+</body>
+</html>
---
+++ iDeal.Example/Default.aspx.cs
@@ -0,0 +1,114 @@
+﻿using iDeal.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace iDeal.Example
+{
+  public partial class Default : System.Web.UI.Page
+  {
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+      EnableButtons();
+    }
+    protected void btnDirReq_Click(object sender, EventArgs e)
+    {
+      try
+      {
+        tableError.Visible = false;
+        logResult.InnerHtml = "";
+
+        var ideal = new iDealService();
+        var response = ideal.SendDirectoryRequest();
+        foreach (var issuer in response.Issuers)
+        {
+          logResult.InnerHtml += String.Format("Issuer: ID={0} Name={1}<br/>", issuer.Id, issuer.Name);
+        }
+        Session["issuers"] = response.Issuers;
+        EnableButtons();
+      }
+
+      catch (iDealException ex)
+      {
+        tableError.Visible = true;
+        lblErrorCode.Text = ex.ErrorCode;
+        lblErrorDetail.Text = ex.ErrorDetail;
+        lblErrorMessage.Text = ex.ErrorMessage;
+        lblConsumerMessage.Text = ex.ConsumerMessage;
+      }
+    }
+    protected void btnTransaction1_Click(object sender, EventArgs e)
+    {
+      try
+      {
+        tableError.Visible = false;
+        logResult.InnerHtml = "";
+
+        var ideal = new iDealService();
+
+        var issuers = (IList<iDeal.Directory.Issuer>)Session["issuers"];
+        int amount = Int32.Parse(txtAmount.Value);
+        var response = ideal.SendTransactionRequest(issuers[0].Id, "http://www.your-url.com", "purchaseId", amount, TimeSpan.FromMinutes(5), "Buy something for " + txtAmount.Value + " euro", "myentrancecode");
+
+        logResult.InnerHtml = String.Format("AcquirerId:{0}<br>", response.AcquirerId);
+        logResult.InnerHtml += String.Format("TransactionId:{0}<br>", response.TransactionId);
+        logResult.InnerHtml += String.Format("PurchaseId:{0}<br>", response.PurchaseId);
+        logResult.InnerHtml += String.Format("<a href='{0}' target=='new'>complete transaction</a><br>", response.IssuerAuthenticationUrl);
+
+        Session["transactionid"] = response.TransactionId;
+        EnableButtons();
+      }
+
+      catch (iDealException ex)
+      {
+        tableError.Visible = true;
+        lblErrorCode.Text = ex.ErrorCode;
+        lblErrorDetail.Text = ex.ErrorDetail;
+        lblErrorMessage.Text = ex.ErrorMessage;
+        lblConsumerMessage.Text = ex.ConsumerMessage;
+      }
+
+    }
+
+    private void EnableButtons()
+    {
+      btnTransaction1.Enabled = (Session["issuers"] != null);
+      btnStatus1.Enabled = (Session["transactionid"] != null);
+    }
+    protected void btnStatus1_Click(object sender, EventArgs e)
+    {
+      try
+      {
+        tableError.Visible = false;
+        logResult.InnerHtml = "";
+
+        var ideal = new iDealService();
+        var response = ideal.SendStatusRequest((string)Session["transactionid"]);
+
+        logResult.InnerHtml += String.Format("Status:{0}<br>", response.Status);
+        logResult.InnerHtml = String.Format("TransactionId:{0}<br>", response.TransactionId);
+        logResult.InnerHtml += String.Format("AcquirerId:{0}<br>", response.AcquirerId);
+        logResult.InnerHtml += String.Format("ConsumerName:{0}<br>", response.ConsumerName);
+        logResult.InnerHtml += String.Format("ConsumerIBAN:{0}<br>", response.ConsumerIBAN);
+        logResult.InnerHtml += String.Format("ConsumerBIC:{0}<br>", response.ConsumerBIC);
+
+        Session["transactionid"] = null;
+        EnableButtons();
+      }
+
+      catch (iDealException ex)
+      {
+        tableError.Visible = true;
+        lblErrorCode.Text = ex.ErrorCode;
+        lblErrorDetail.Text = ex.ErrorDetail;
+        lblErrorMessage.Text = ex.ErrorMessage;
+        lblConsumerMessage.Text = ex.ConsumerMessage;
+      }
+
+    }
+  }
+}
